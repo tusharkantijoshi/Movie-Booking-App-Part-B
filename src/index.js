68121ec1cb95489a79e2d435134cdb0c81199a56
Tusharkanti <tusharkantijoshi@gmail.com>
2022-06-08 16:57:12 +0530

    Checkpoint 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'typeface-roboto';
-import registerServiceWorker from './registerServiceWorker';
-import Controller from './screens/Controller';
 
-ReactDOM.render(<Controller />, document.getElementById('root'));
-registerServiceWorker();
+import Home from './screens/home/Home'
+import Details from './screens/details/Details'
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='details' element={<Details />} />
+      </Routes>
+    </BrowserRouter>,
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/screens/home/Home.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "./Home.css";
+import "../../common/header/Header";
+import Header from "../../common/header/Header.js";
+import SingleLineImageList from "./movieList";
+import moviesData from "../../common/moviesData";
+import TitlebarImageList from "./leftImageList";
+import SimpleCard, { userSelection } from "./filters";
+import genres from "./genre";
+import artists from "./artists";
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: moviesData,
+      genres: genres,
+      artists: artists,
+      userSelection: moviesData,
+    };
+  }
+
+  filterHandler = () => {
+    if (
+      userSelection.name === "" &&
+      userSelection.releaseDateEnd === "" &&
+      userSelection.releaseDateStart === "" &&
+      userSelection.genres.length === 0 &&
+      userSelection.artists.length === 0
+    ) {
+      const state = this.state;
+      state.userSelection = moviesData;
+      this.setState(state);
+      return moviesData;
+    }
+
+    else {
+      const filteredMovies = this.state.data.filter((movie) => {
+        if (
+          movie.title.toLowerCase() === userSelection.name.toLowerCase() ||
+          movie.genres.some((genre) => userSelection.genres.includes(genre)) || parseInt(new Date(movie.release_date).getTime()) <= parseInt(new Date(userSelection.releaseDateEnd).getTime()) ||
+          parseInt(new Date(movie.release_date).getTime()) >= parseInt(new Date(userSelection.releaseDateStart).getTime()) || movie.artists.some((artist) =>
+            userSelection.artists.includes(
+              `${artist.first_name} ${artist.last_name}`
+            )
+          )
+        ) {
+          console.log(userSelection.releaseDateStart);
+          console.log(parseInt(new Date(movie.release_date).getTime()) > parseInt(new Date(userSelection.releaseDateEnd).getTime()))
+          return movie;
+        }
+        else
+          return null;
+      });
+
+      const state = this.state;
+      state.userSelection = filteredMovies;
+
+      this.setState(state);
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <Header btnType="loginbtn" variant="contained" buttonName="LOGIN" btnType2="hiddenbtn" buttonName2="hidden" btnType3="logoutbtn" buttonName3="LOGOUT" />
+        <span className="heading">Upcoming Movies</span>
+        <SingleLineImageList moviesData={this.state.data} />
+
+        <div className="flex-container">
+          <div className="left">
+            <TitlebarImageList moviesData={this.state.userSelection} />
+          </div>
+          <div className="right">
+            <SimpleCard
+              genres={this.state.genres}
+              artists={this.state.artists}
+              filterHandler={this.filterHandler}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
